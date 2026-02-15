742a11a0b964dd6425d32df70dbe5f1253ddb445
Arraso Promo <arraso.promo@gmail.com>
2026-02-15 17:10:03 -0300

    bale

--- server.js
+++ server.js
@@ -184,7 +184,7 @@ app.get('/apresentacao', (req, res) => res.sendFile(path.join(__dirname, 'views'
 app.get('/aulas', (req, res) => res.sendFile(path.join(__dirname, 'views', 'aulas.html')));
 app.get('/novidade', (req, res) => res.sendFile(path.join(__dirname, 'views', 'novidade.html')));
 app.get('/upgrade', (req, res) => res.sendFile(path.join(__dirname, 'views', 'upgrade.html')));
-app.get('/ferramentas', (req, res) => res.sendFile(path.join(__dirname, 'views', 'fluxo.html')));
+app.get('/ferramentas', (req, res) => res.sendFile(path.join(__dirname, 'views', 'ferramentas.html')));
 
 // Redirects for direct access
 app.get('/ingredientes', (req, res) => res.redirect('/precificacao?tab=ingredientes'));
